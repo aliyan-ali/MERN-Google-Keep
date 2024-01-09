2b4e33677f9f82775b438ac4183d0962b4acfd12
aliyan-ali <aliyan.zunati1020@gmail.com>
2024-01-09 14:55:29 +0500

    added a confirm passord on signup

--- Frontend/google-keep/src/app/components/molecules/SigninForm.jsx
+++ Frontend/google-keep/src/app/components/molecules/SigninForm.jsx
@@ -4,21 +4,21 @@ import arrowsvg from "../atoms/img/arrow.svg";
 import SigninEmailInput from "../atoms/SigninEmailInput";
 import SigninPassword from "../atoms/SigninPassword";
 import style from "./SigninForm.css";
+import RepeatPassword from "../atoms/RepeatPassword";
 
 function SigninForm({
   name,
   email,
   password,
+  repeatPassword,
   setName,
   setEmail,
   setPassword,
+  setRepeatPassword,
   onSubmit,
   router,
-}) { {
-  /* adding name for user */
-}
-
-
+  formError
+}) {
   const handleSubmit = (e) => {
     e.preventDefault();
     onSubmit(); // Call the onSubmit function passed as a prop
@@ -39,15 +39,30 @@ function SigninForm({
 
           <SigninEmailInput email={email} setEmail={setEmail} />
           <SigninPassword password={password} setPassword={setPassword} />
+          <RepeatPassword
+            repeatPassword={repeatPassword}
+            setRepeatPassword={setRepeatPassword}
+          />
         </div>
         <div>
-          <button name="submitSignin" className="btn signin-btn" type="submit">
+          <button
+            name="submitSignin"
+            className={`btn signin-btn ${
+              formError === "Passords-not-match" && "red"
+            }`}
+            type="submit"
+          >
             <p>Signup</p>
             <Image src={arrowsvg} width={30} height={25} alt=">" />
           </button>
         </div>
         <div>
-          <p className="p">Allready have an account?</p>
+          <div className="check-err">
+            <p className="p">Allready have an account?</p>
+            {formError === "Passords-not-match" && (
+              <p className="red">Passwords not match</p>
+            )}
+          </div>
           <button
             name="navigateSignin"
             className="btn signup-btn"
